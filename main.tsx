22aa848f3e86032f98b3ee31783030410b78ee90
Mapboro <mapboro@gmail.com>
2025-11-20 23:31:52 -0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,10 +1,9 @@
-// main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 ).render(
   <React.StrictMode>
     <App />
